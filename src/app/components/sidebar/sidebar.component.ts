e0401b4218f8a397f906e82c5a7ffa5bc73fe24d
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2024-02-18 13:54:07 +0100

    submenu

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -6,16 +6,30 @@ declare interface RouteInfo {
     title: string;
     icon: string;
     class: string;
+    children?: RouteInfo[];
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
+    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: ''  },
     { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
     { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
-    { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
+    { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '', },
     { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
     { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
     { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' },
-    { path: '/user-management', title: 'Gestion des utilisateurs',  icon:'ni-circle-08 text-pink', class: '' }
+    { path: '/user-management', title: 'Gestion des utilisateurs',icon:'ni-circle-08 text-pink',class: '',children: [
+      {
+        path: '/user-management/ajouter-utilisateur',
+        title: 'Ajouter un utilisateur',
+        icon: '',
+        class: ''
+      },
+      {
+        path: '/user-management/afficher utilisateurs',
+        title: 'Afficher les utilisateurs',
+        icon: '',
+        class: ''
+      }
+    ]}
 ];
 
 @Component({
@@ -36,4 +50,8 @@ export class SidebarComponent implements OnInit {
       this.isCollapsed = true;
    });
   }
+  toggleSubmenu(menuItem: any) {
+  menuItem.showSubmenu = !menuItem.showSubmenu;
+}
+
 }
