8f9d2dc44d5087e62bccbfb9382d4cfe46a94687
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2024-02-18 18:11:46 +0100

    usr form ajout

--- src/app/pages/gestiondesutilisateur/ajouter-utilisateur/ajouter-utilisateur.component.ts
+++ src/app/pages/gestiondesutilisateur/ajouter-utilisateur/ajouter-utilisateur.component.ts
@@ -3,7 +3,8 @@ import { ERole } from 'src/app/model/erole';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { technologies } from 'src/app/model/Technologies';
+import { technologies } from 'src/app/model/technologies';
+
 @Component({
   selector: 'app-ajouter-utilisateur',
   templateUrl: './ajouter-utilisateur.component.html',
