c38128291b50cbbe2944933f1ace530f36d4cdb9
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2024-04-25 17:13:40 +0100

    add projet tache gestion user

--- src/app/pages/gestiondesutilisateur/gestiondesutilisateur.component.ts
+++ src/app/pages/gestiondesutilisateur/gestiondesutilisateur.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Projet } from 'src/app/model/Projet';
+import { Tache } from 'src/app/model/tache';
 import { User } from 'src/app/model/user';
 import { CompetenceService } from 'src/app/service/competence.service';
+import { ProjetServiceService } from 'src/app/service/projet-service.service';
+import { TacheserviceService } from 'src/app/service/tacheservice.service';
 import { UserServiceService } from 'src/app/service/user-service.service';
 
 @Component({
@@ -15,22 +19,71 @@ export class GestiondesutilisateurComponent implements OnInit {
 
   users:User[];
   initialRating: number = 5;
-
-  constructor(private us:UserServiceService,private toastrService: ToastrService
+  taches: Tache[]=[];
+  tasks: any[];
+  projet:Projet[]=[];
+  constructor(private us:UserServiceService,private toastrService: ToastrService,
+    private ts:TacheserviceService,private ps:ProjetServiceService
     ) { }
 
   ngOnInit(): void {
     this.getusers();
 
   }
-getusers(){
-  this.us.getusers().subscribe(
-    data=>{
-
-      this.users=data;
-    }
-  )
-}
+  gettachebuuserid(userId: Number, index: number) { // Add index parameter
+    this.ts.gettachebyuserId(userId).subscribe(
+      data => {
+        this.tasks = data;
+        for (let task of data) {
+          if (task.status === 'en cours') {
+            this.affichetachedetail(task.id.tacheId, index); // Pass index to affichetachedetail function
+            console.log(task);
+            this.getprojetbytacheid(task.id.tacheId, index); // Pass index to getprojetbytacheid function
+          }
+        }
+        console.log(this.projet);
+        console.log(this.taches);
+      }
+    );
+  }
+  
+  getprojetbytacheid(tacheid: number, index: number) { // Add index parameter
+    this.ps.getprojettachebyid(tacheid).subscribe(
+      projet => {
+        this.projet[index] = projet; // Assign the project to the correct index
+      }
+    );
+  }
+  
+  affichetachedetail(tacheid: number, index: number) { // Add index parameter
+    this.ts.gettachebyId(tacheid).subscribe(
+      res => {
+        this.taches[index] = res; // Assign the task to the correct index
+      }
+    );
+  }
+  getusers() {
+    this.us.getusers().subscribe(
+      data => {
+        console.log(data);
+        this.users = data;
+  
+        // Initialize taches and projet lists with null values
+        this.taches = new Array(data.length).fill(null);
+        this.projet = new Array(data.length).fill(null);
+  
+        for (let i = 0; i < data.length; i++) {
+          const u = data[i];
+          if (u.status === 'non disponible') {
+            this.gettachebuuserid(u.id, i); // Pass index i to gettachebuuserid function
+          } else if (u.status == null || u.status === 'disponible') {
+            this.taches[i] = null;
+            this.projet[i] = null;
+          }
+        }
+      }
+    );
+  }
 suprimer(user :any){
   this.us.deleteUser(user.id).subscribe(
     res=>{
